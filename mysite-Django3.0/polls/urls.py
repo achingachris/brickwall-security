6cfc85908fadd70e484c44e1c97fdf36e8e84978
ChrisAchinga <achinga.chris@gmail.com>
2020-04-28 16:31:30 +0300

    /

--- mysite-Django3.0/polls/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views as v
-
-urlpatterns = [
-    path('hello/', v.HelloWorld, name='hello')
-]
--- mysite-Django3.0/polls/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def HelloWorld(request):
-    return HttpResponse("Ohh Hey I will not use that Hello World crap, lets stick to OOOh!!! It is working")
